2d45df9fb83f4834af34d2f5c9ffd2036aaf6ab0
RalphAdi <153388309+RalphAdi@users.noreply.github.com>
2024-01-22 17:28:39 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup() {
   World.add(world, tower);
 
   cannon = new Cannon(180, 110, 130, 100, angle);
-  boat = new Boat(width-79, height - 60, 170, 170,-80);
+ 
 }
 
 function draw() {
@@ -36,19 +36,18 @@ function draw() {
 
   Engine.update(engine);
 
-  
-  rect(ground.position.x, ground.position.y, width * 2, 1);
  
+  rect(ground.position.x, ground.position.y, width * 2, 1);
+  
 
-  push();  
+  push();
   imageMode(CENTER);
   image(towerImage,tower.position.x, tower.position.y, 160, 310);
   pop();
 
 
-  Matter.Body.setVelocity(boat.body,{x:-0.9, y:0})
-  boat.display()
-  
+
+  showBoats();
 
   for (var i = 0; i < balls.length; i++) {
     showCannonBalls(balls[i], i);
@@ -72,7 +71,34 @@ function showCannonBalls(ball, index) {
   }
 }
 
+function showBoats() {
+  if (boats.length > 0) {
+    if (
+      boats[boats.length - 1] === undefined ||
+      boats[boats.length - 1].body.position.x < width - 300
+    ) {
+      var positions = [-40, -60, -70, -20];
+      var position = random(positions);
+      var boat = new Boat(width, height - 100, 170, 170, position);
 
+      boats.push(boat);
+    }
+
+    for (var i = 0; i < boats.length; i++) {
+      if (boats[i]) {
+        Matter.Body.setVelocity(boats[i].body, {
+          x: -0.9,
+          y: 0
+        });
+
+        boats[i].display();
+      } 
+    }
+  } else {
+    var boat = new Boat(width, height - 60, 170, 170, -60);
+    boats.push(boat);
+  }
+}
 
 function keyReleased() {
   if (keyCode === DOWN_ARROW) {
